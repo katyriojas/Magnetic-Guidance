aba00636318723c887c96e58b966200a556cc14d
Unknown <t.bruns@vanderbilt.edu>
2019-09-10 14:03:38 -0500

    Updated colorbar plot

--- MagneticGuidanceColorbars.m
+++ MagneticGuidanceColorbars.m
@@ -1,102 +1,171 @@
 %% Load data
 
 % guided/unguided phantom data
-% load('data\phantom\data_phantom.mat');
+if ~exist('data', 'var')
+    load('data\phantom\data_phantom.mat'); % loads 'data'
+end
 
 % manual phantom data
-load('data\phantom\ManualPhantomData.mat');
+if ~exist('pman', 'var')
+    load('data\phantom\ManualPhantomData.mat'); % load 'pman'
+end
+
+N = length(data); % assumes equal # of manual, unguided, and guided trials
+
+% trim to 10 deg from final angular depth
+for ii=1:N
+    trim_ind_manual = find(pman(ii).angdepth > (pman(ii).angdepth(end)-10), 1);
+    trim_ind_nomag  = find(data(ii).nomag_mea_interp_angdepth > (data(ii).nomag_mea_interp_angdepth(end)-10), 1);
+    trim_ind_mag    = find(data(ii).mag_interp_angdepth > (data(ii).mag_interp_angdepth(end)-10), 1);
+
+    phantom(ii).manual.angdepth = pman(ii).angdepth(1:trim_ind_manual);
+    phantom(ii).manual.Fmag_smooth = pman(ii).Fmag_smooth(1:trim_ind_manual);
+
+    phantom(ii).nomag.angdepth = data(ii).nomag_mea_interp_angdepth(1:trim_ind_nomag);
+    phantom(ii).nomag.Fmag_smooth = data(ii).nomag_mea.Fmag_smooth(1:trim_ind_nomag);
+
+    phantom(ii).mag.angdepth = data(ii).mag_interp_angdepth(1:trim_ind_mag);
+    phantom(ii).mag.Fmag_smooth = data(ii).mag.Fmag_smooth(1:trim_ind_mag);
+end
 
 
 %% Create base profile (circle/square)
 
-base_type = 'circle';
+base_type = 'square';
 width = 20;
-spacing = 1.02*width;
 
 if strcmp(base_type, 'square') 
     q = linspace(0,2*pi,5) + pi/4;
     base = (width/2)*[cos(q); sin(q)];   % Base curve is a square
+    spacing = .7*width;
+    cases_gap = 0.15*width;
 elseif strcmp(base_type, 'circle')
     % Base curve is a circle
     cir_res = 40;   % circle resolution (# pts)
     q = linspace(0, 2*pi, cir_res);
-    base = (width/2)*[cos(q); sin(q)]; 
+    base = (width/2)*[cos(q); sin(q)];
+    spacing = 1.02*width;
+    cases_gap = 0.2*width;
 else
     error('base_type must be ''circle'' or ''square''')
 end
   
 %% create paths
 
-% trim 10 degrees back from final angular depth
-% trim_deg = 10;
-% mag1_end = mag1.interp_angdepth(end)-trim_deg;
-% nomag1_end = nomag1.interp_angdepth(end)-trim_deg;
-% mag1_end_ind = find(mag1.interp_angdepth >= mag1_end, 1);
-% nomag1_end_ind = find(nomag1.interp_angdepth >= nomag1_end, 1);
-% max_ang_index1 = find(st_theta > mag1_end, 1);
-% max_ang_index2 = find(st_theta > nomag1_end, 1);
+N = length(data);
+y_locations = linspace(3*spacing*N, 0, 3*N) - 1.5*spacing*N;
+y_locations = y_locations + [cases_gap*ones(1,N), zeros(1,N), -cases_gap*ones(1,N)];
 
+for ii=1:N
+    path(ii).manual = [phantom(ii).manual.angdepth,...
+                       y_locations(ii)*ones(size(phantom(ii).manual.angdepth)),...
+                       zeros(size(phantom(ii).manual.angdepth))]';
 
-for ii=1:length(data)
-    path(ii).mag   = [data(ii).mag_interp_angdepth,  (ii*spacing-width/3)*ones(size(data(ii).mag_interp_angdepth)), zeros(size(data(ii).mag_interp_angdepth))]';
-    path(ii).nomag = [data(ii).nomag_mea_interp_angdepth,  (-ii*spacing+width/3)*ones(size(data(ii).nomag_mea_interp_angdepth)), zeros(size(data(ii).nomag_mea_interp_angdepth))]';
+    path(ii).nomag  = [phantom(ii).nomag.angdepth,...
+                       y_locations(ii+N)*ones(size(phantom(ii).nomag.angdepth)),...
+                       zeros(size(phantom(ii).nomag.angdepth))]';
+
+    path(ii).mag    = [phantom(ii).mag.angdepth,...
+                      y_locations(ii+N*2)*ones(size(phantom(ii).mag.angdepth)),...
+                      zeros(size(phantom(ii).mag.angdepth))]';
 end
 
 %% extrude path to create surface
 
-for ii=1:length(data)
-
-    [X(ii).mag, Y(ii).mag, Z(ii).mag] = extrude(base, path(ii).mag, 0);
-    [X(ii).nomag, Y(ii).nomag, Z(ii).nomag] = extrude(base, path(ii).nomag, 0);
+for ii=1:N
+    [X(ii).manual, Y(ii).manual, Z(ii).manual] = extrude(base, path(ii).manual, 0);
+    [X(ii).mag,    Y(ii).mag,    Z(ii).mag]    = extrude(base, path(ii).mag, 0);
+    [X(ii).nomag,  Y(ii).nomag,  Z(ii).nomag]  = extrude(base, path(ii).nomag, 0);
 end
 
 %% plot
 figure(13); clf(13); 
-axis equal; 
+% axis equal; 
 hold on; 
-% grid off;
+% grid on;
 
-% plot extrusions
-for ii=1:length(data)
+
+% plot extrusions and map forces to CData
+for ii=1:N
+    h(ii).manual = surf(X(ii).manual, Y(ii).manual, Z(ii).manual);
+    h(ii).manual.CData = repmat(phantom(ii).manual.Fmag_smooth', [length(base), 1]);
+
     h(ii).mag = surf(X(ii).mag, Y(ii).mag, Z(ii).mag);
-    h(ii).mag.CData = repmat(data(ii).mag.Fmag_smooth', [length(base), 1]);
+    h(ii).mag.CData = repmat(phantom(ii).mag.Fmag_smooth', [length(base), 1]);
 
     h(ii).nomag = surf(X(ii).nomag, Y(ii).nomag, Z(ii).nomag);
-    h(ii).nomag.CData = repmat(data(ii).nomag_mea.Fmag_smooth', [length(base), 1]);
+    h(ii).nomag.CData = repmat(phantom(ii).nomag.Fmag_smooth', [length(base), 1]);
 end
 
 
+% add case labels
+text_locations = [30*ones(size(y_locations)); y_locations; 1.05*width*ones(size(y_locations))]';
+% labels = {'Case 1, Trial 1', 'Case 1, Trial 2', 'Case 1, Trial 3', 'Case 1, Trial 4',...
+%           'Case 2, Trial 1', 'Case 2, Trial 2', 'Case 2, Trial 3', 'Case 2, Trial 4',...
+%           'Case 3, Trial 1', 'Case 3, Trial 2', 'Case 3, Trial 3', 'Case 3, Trial 4'  };
+
+labels = {'Trial 1', 'Trial 2', 'Trial 3', 'Trial 4','Trial 1', 'Trial 2', 'Trial 3', 'Trial 4','Trial 1', 'Trial 2', 'Trial 3', 'Trial 4'};
+
+for ii=1:length(text_locations)
+    text('String',labels(ii), 'Position',text_locations(ii,:), 'FontSize',14, 'Color','w' );
+end
+
+
+% find max force
+temp = [phantom.manual];
+forces = vertcat(temp.Fmag_smooth);
+temp = [phantom.nomag];
+forces = vertcat(forces, temp.Fmag_smooth);
+temp = [phantom.mag];
+forces = vertcat(forces, temp.Fmag_smooth);
+max_force = max(forces);
+
+% colormap
+load('forces_colormap.mat'); % loads forces_colormap
+cmap = colormap(forces_colormap);
+
+% cmap_colors = hot(ceil(max_force) + 70);
+% cmap_colors = cmap_colors(1:end-70,:);
+% cmap = colormap(cmap_colors);
+
+% % rescale colormap
+% cMap = hot(256+70);
+% % cMap = cool(256);
+% cMap = cMap(1:end-80,:);
+% dataMax = max_force;
+% dataMin = 0;
+% centerPoint = 30;
+% scalingIntensity = 4;
+% 
+% x = 1:length(cMap); 
+% x = x - (centerPoint-dataMin)*length(x)/(dataMax-dataMin);
+% x = scalingIntensity * x/max(abs(x));
+% x = sign(x).* exp(abs(x));
+% x = x - min(x); x = x*511/max(x)+1; 
+% newMap = interp1(x, cMap, 1:512);
+% cmap_scaled = colormap(newMap);
+
+
+ax = gca;
+ax.FontSizeMode = 'manual';
+ax.FontSize = 14;
+ax.XTick = 0:90:630;
+ax.YTick = [];
+ax.ZTick = [];
+xlabel('Angular Insertion Depth (deg)', 'FontSize',14)
+xlim([10 630])
+
+set(gca,'ytick',[])
+set(gca,'yticklabel',[])
+ylim([min(y_locations)-width/2, max(y_locations)+width/2])
+
+set(gca,'ztick',[])
+set(gca,'zticklabel',[])
 
 shading flat
 view(0,90)
 
-% rescale colormap
-cMap = hot(256+70);
-% cMap = cool(256);
-cMap = cMap(1:end-70,:);
-dataMin = 0;
-dataMax = 100;
-% dataMax = max(data_mag1.Fmag_smooth);
-% dataMin = min(data_mag1.Fmag_smooth);
-centerPoint = 5;
-scalingIntensity = 5;
-
-x = 1:length(cMap); 
-x = x - (centerPoint-dataMin)*length(x)/(dataMax-dataMin);
-x = scalingIntensity * x/max(abs(x));
-x = sign(x).* exp(abs(x));
-x = x - min(x); x = x*511/max(x)+1; 
-newMap = interp1(x, cMap, 1:512);
-cmap_scaled = colormap(newMap);
-
-xlabel('Angular Insertion Depth (deg)')
-% set(gca,'xtick',[])
-% set(gca,'xticklabel',[])
-set(gca,'ytick',[])
-set(gca,'yticklabel',[])
-set(gca,'ztick',[])
-set(gca,'zticklabel',[])
-
-cbar = colorbar;
+cbar = colorbar('Limits', [0 max_force]);
+% cbar.Ticks = 
 cbar.Label.String = 'Force (mN)';
 cbar.Label.FontSize = 14';
