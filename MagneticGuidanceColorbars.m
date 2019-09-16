eea81b147eb0ad7e1651aa6948568ebc870c9fc0
Unknown <t.bruns@vanderbilt.edu>
2019-09-16 08:41:43 -0500

    TLB: last commit

--- MagneticGuidanceColorbars.m
+++ MagneticGuidanceColorbars.m
@@ -32,7 +32,7 @@ end
 %% Create base profile (circle/square)
 
 base_type = 'square';
-width = 20;
+width = 50;
 
 if strcmp(base_type, 'square') 
     q = linspace(0,2*pi,5) + pi/4;
@@ -49,12 +49,11 @@ elseif strcmp(base_type, 'circle')
 else
     error('base_type must be ''circle'' or ''square''')
 end
-  
-%% create paths
 
-N = length(data);
 y_locations = linspace(3*spacing*N, 0, 3*N) - 1.5*spacing*N;
 y_locations = y_locations + [cases_gap*ones(1,N), zeros(1,N), -cases_gap*ones(1,N)];
+  
+%% create paths
 
 for ii=1:N
     path(ii).manual = [phantom(ii).manual.angdepth,...
@@ -100,15 +99,11 @@ end
 
 % add case labels
 text_locations = [30*ones(size(y_locations)); y_locations; 1.05*width*ones(size(y_locations))]';
-% labels = {'Case 1, Trial 1', 'Case 1, Trial 2', 'Case 1, Trial 3', 'Case 1, Trial 4',...
-%           'Case 2, Trial 1', 'Case 2, Trial 2', 'Case 2, Trial 3', 'Case 2, Trial 4',...
-%           'Case 3, Trial 1', 'Case 3, Trial 2', 'Case 3, Trial 3', 'Case 3, Trial 4'  };
-
 labels = {'Trial 1', 'Trial 2', 'Trial 3', 'Trial 4','Trial 1', 'Trial 2', 'Trial 3', 'Trial 4','Trial 1', 'Trial 2', 'Trial 3', 'Trial 4'};
 
-for ii=1:length(text_locations)
-    text('String',labels(ii), 'Position',text_locations(ii,:), 'FontSize',14, 'Color','w' );
-end
+% for ii=1:length(text_locations)
+%     text('String',labels(ii), 'Position',text_locations(ii,:), 'FontSize',14, 'Color','w' );
+% end
 
 
 % find max force
@@ -121,29 +116,8 @@ forces = vertcat(forces, temp.Fmag_smooth);
 max_force = max(forces);
 
 % colormap
-load('forces_colormap.mat'); % loads forces_colormap
+load('forces_colormap.mat'); % loads forces_colormap (use colormapeditor to create/edit)
 cmap = colormap(forces_colormap);
-
-% cmap_colors = hot(ceil(max_force) + 70);
-% cmap_colors = cmap_colors(1:end-70,:);
-% cmap = colormap(cmap_colors);
-
-% % rescale colormap
-% cMap = hot(256+70);
-% % cMap = cool(256);
-% cMap = cMap(1:end-80,:);
-% dataMax = max_force;
-% dataMin = 0;
-% centerPoint = 30;
-% scalingIntensity = 4;
-% 
-% x = 1:length(cMap); 
-% x = x - (centerPoint-dataMin)*length(x)/(dataMax-dataMin);
-% x = scalingIntensity * x/max(abs(x));
-% x = sign(x).* exp(abs(x));
-% x = x - min(x); x = x*511/max(x)+1; 
-% newMap = interp1(x, cMap, 1:512);
-% cmap_scaled = colormap(newMap);
 
 
 ax = gca;
@@ -151,7 +125,6 @@ ax.FontSizeMode = 'manual';
 ax.FontSize = 14;
 ax.XTick = 0:90:630;
 ax.YTick = [];
-ax.ZTick = [];
 xlabel('Angular Insertion Depth (deg)', 'FontSize',14)
 xlim([10 630])
 
@@ -159,13 +132,11 @@ set(gca,'ytick',[])
 set(gca,'yticklabel',[])
 ylim([min(y_locations)-width/2, max(y_locations)+width/2])
 
-set(gca,'ztick',[])
-set(gca,'zticklabel',[])
+% set(gca,'ztick',[])
+% set(gca,'zticklabel',[])
 
 shading flat
-view(0,90)
 
 cbar = colorbar('Limits', [0 max_force]);
-% cbar.Ticks = 
 cbar.Label.String = 'Force (mN)';
 cbar.Label.FontSize = 14';
